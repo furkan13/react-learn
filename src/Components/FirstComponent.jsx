3dc187725b84df6bc7f3efcc85b654eaa67847da
Furkan Workstation <falahi161212@bscse.uiu.ac.bd>
2023-03-22 21:17:08 +0000

    Topic covered - Component,Module,Export

---
+++ src/Components/FirstComponent.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class FirstComponent extends Component
+{
+    render()
+    {
+        return(
+            <div className="FirstComponent">
+                2nd one
+            </div>
+
+        );
+    }
+}
